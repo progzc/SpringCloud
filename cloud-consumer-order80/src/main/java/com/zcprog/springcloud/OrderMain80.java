a2681e74f3c9d5590e394093a59fcd594911a8ce
progzc <zcprog@foxmail.com>
2021-01-13 22:13:28 +0800

    cloud-consumer-order80：完成消费时的负载均衡

--- cloud-consumer-order80/src/main/java/com/zcprog/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/zcprog/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.zcprog.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description 消费者主启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version V1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/zcprog/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/zcprog/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.zcprog.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
+    @LoadBalanced // 使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
