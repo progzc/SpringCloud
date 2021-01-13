378203ab9b0cc6fce87837b7767c79bc45e9a036
progzc <zcprog@foxmail.com>
2021-01-13 21:17:32 +0800

    cloud-eureka-server7001

---
+++ cloud-eureka-server7001/src/main/java/com/zcprog/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.zcprog.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description Eureka注册中心
+ * @Author zhaochao
+ * @Date 2021/1/13 18:22
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
