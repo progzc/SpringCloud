ad3037f68ff377f3a1258ddef162753ed520bee5
progzc <zcprog@foxmail.com>
2021-01-15 19:00:37 +0800

    cloud-provider-hystrix-payment8001

--- cloud-provider-hystrix-payment8001/src/main/java/com/zcprog/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zcprog/springcloud/PaymentHystrixMain8001.java
@@ -1,8 +1,12 @@
 package com.zcprog.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Description 主启动类
@@ -13,8 +17,25 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+
+    /**
+     * 新版本Hystrix需要在主启动类MainAppHystrix8001中指定监控路径
+     * 此配置是为了服务监控而配置，与服务容错本身无关，是SpringCLoud升级后的坑
+     * ServletRegistrationBean:因为SpringBoot的默认路径不是"/hystrix.stream",
+     * 只要在自己的项目里配置上下面的servlet就可以了
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/zcprog/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zcprog/springcloud/controller/PaymentController.java
@@ -27,15 +27,24 @@ public class PaymentController {
     private String serverPort;
 
     @GetMapping("/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id){
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
         String result = paymentService.paymentInfo_OK(id);
-        log.info("*******result:"+result);
+        log.info("*******result:" + result);
         return result;
     }
+
     @GetMapping("/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentService.paymentInfo_TimeOut(id);
-        log.info("*******result:"+result);
+        log.info("*******result:" + result);
+        return result;
+    }
+
+    //服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("*******result:" + result);
         return result;
     }
 }
