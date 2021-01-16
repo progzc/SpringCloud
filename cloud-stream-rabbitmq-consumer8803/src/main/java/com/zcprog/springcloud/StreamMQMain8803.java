1aa50b3600baf82c7f0666881bfe3d56920ae611
progzc <zcprog@foxmail.com>
2021-01-16 22:20:19 +0800

    cloud-stream-rabbitmq-consumer8803

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/zcprog/springcloud/StreamMQMain8803.java
@@ -0,0 +1,21 @@
+package com.zcprog.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description 主启动类
+ * @Author zhaochao
+ * @Date 2021/1/16 21:20
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
+
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/zcprog/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,25 @@
+package com.zcprog.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+
+/**
+ * @Description 控制器类
+ * @Author zhaochao
+ * @Date 2021/1/16 21:22
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者2号，接受："+message.getPayload()+"\t port:"+serverPort);
+    }
+}
