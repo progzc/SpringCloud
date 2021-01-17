9c635a91c1f3770c24b0a14b098601c084c600f7
progzc <zcprog@foxmail.com>
2021-01-17 13:37:28 +0800

    cloudalibaba-consumer-nacos-order83

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/zcprog/springcloud/alibaba/OrderNacosMain83.java
@@ -0,0 +1,20 @@
+package com.zcprog.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description 主启动类
+ * @Author zhaochao
+ * @Date 2021/1/17 11:15
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/zcprog/springcloud/alibaba/controller/OrderNacosController.java
@@ -0,0 +1,33 @@
+package com.zcprog.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description 控制器
+ * @Author zhaochao
+ * @Date 2021/1/17 11:21
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@RestController
+@Slf4j
+public class OrderNacosController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+}
+
