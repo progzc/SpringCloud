4ed74634d31d40c14edbd403e51944fdeac7da6c
progzc <zcprog@foxmail.com>
2021-01-17 15:54:00 +0800

    cloudalibaba-config-nacos-client3377

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/zcprog/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,20 @@
+package com.zcprog.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description 主启动类
+ * @Author zhaochao
+ * @Date 2021/1/17 13:46
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
