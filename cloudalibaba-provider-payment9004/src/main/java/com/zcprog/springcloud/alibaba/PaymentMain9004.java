de22ea9310c6e616bfc606c3f5c6db485e76ea80
progzc <zcprog@foxmail.com>
2021-01-18 14:29:08 +0800

    cloudalibaba-provider-payment9004

---
+++ cloudalibaba-provider-payment9004/src/main/java/com/zcprog/springcloud/alibaba/PaymentMain9004.java
@@ -0,0 +1,20 @@
+package com.zcprog.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description 主启动类
+ * @Author zhaochao
+ * @Date 2021/1/18 11:47
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+}
